5725280ca8cc0094f36771d52494dd60be2de2f0
Arthur Carvalho <arthur.carvalho.tech@gmail.com>
2024-04-15 20:56:36 -0300

    My first commit

---
+++ src/test/java/com/jsp/SpringBootCRUD/SpringBootCrudApplicationTests.java
@@ -0,0 +1,9 @@
+package com.jsp.SpringBootCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudApplicationTests {
+
+}
